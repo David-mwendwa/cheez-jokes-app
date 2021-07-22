41bdde957eed91c501103631430fd6948a42168b
David Mwendwa <klatzdavie19@gmail.com>
2021-07-22 11:58:17 +0300

    feat: add dynamic emojis

--- src/Joke.js
+++ src/Joke.js
@@ -20,13 +20,23 @@ class Joke extends Component {
     }
   };
 
-  'em em-rolling_on_the_floor_loughing';
-  'em em-loughing';
-  'em em-smiley';
-  'em em-slightly_smiling_face';
-  'em em-neutral_face';
-  'em em-confused';
-  'em em-angry';
+  getEmoji = () => {
+    if (this.props.votes >= 15) {
+      return 'em em-rolling_on_the_floor_laughing';
+    } else if (this.props.votes >= 12) {
+      return 'em em-laughing';
+    } else if (this.props.votes >= 9) {
+      return 'em em-smiley';
+    } else if (this.props.votes >= 6) {
+      return 'em em-slightly_smiling_face';
+    } else if (this.props.votes >= 3) {
+      return 'em em-neutral_face';
+    } else if (this.props.votes >= 0) {
+      return 'em em-confused';
+    } else  {
+      return 'em em-angry';
+    }
+  }
 
   render() {
     return (
@@ -39,6 +49,9 @@ class Joke extends Component {
           <i className='fas fa-arrow-down' onClick={this.props.downvote} />
         </div>
         <div className='Joke-text'>{this.props.text}</div>
+        <div className='joke-smiley'>
+          <i className={this.getEmoji()} />
+        </div>
       </div>
     );
   }
