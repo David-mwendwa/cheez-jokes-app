18589349868fec148080803042615842a93c3357
David Mwendwa <klatzdavie19@gmail.com>
2021-07-22 13:10:20 +0300

    feat: prevent duplicate jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,32 +11,49 @@ class JokeList extends Component {
     numJokesToGet: 10,
   };
 
-  state = {
-    jokes: JSON.parse(window.localStorage.getItem('jokes')) || [],
-    loading: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: JSON.parse(window.localStorage.getItem('jokes')) || [],
+      loading: false,
+    };
+
+    this.seenJokes = new Set(this.state.jokes.map((j) => j.text));
+    console.log(this.seenJokes);
+  }
 
   componentDidMount() {
     if (this.state.jokes.length === 0) this.getJokes();
   }
 
   getJokes = async () => {
-    let jokes = [];
-    while (jokes.length < this.props.numJokesToGet) {
-      let res = await axios.get('https://icanhazdadjoke.com/', {
-        headers: { Accept: 'application/json' },
-      });
-      jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
-    }
+    try {
+      let jokes = [];
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get('https://icanhazdadjoke.com/', {
+          headers: { Accept: 'application/json' },
+        });
+        let newJoke = res.data.joke;
+        if (!this.seenJokes.has(newJoke)) {
+          jokes.push({ id: uuidv4(), text: newJoke, votes: 0 });
+        } else {
+          console.log('FOUND A DUPLICATE');
+          console.log(newJoke);
+        }
+      }
 
-    this.setState(
-      (st) => ({
-        loading: false,
-        jokes: [...st.jokes, ...jokes],
-      }),
-      () =>
-        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
-    );
+      this.setState(
+        (st) => ({
+          loading: false,
+          jokes: [...st.jokes, ...jokes],
+        }),
+        () =>
+          window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+      );
+    } catch (err) {
+      alert(err)
+      this.setState({loading: false})
+    }
   };
 
   handleVote = (id, delta) => {
